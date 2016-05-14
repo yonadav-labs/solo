44aa819ba3b9374fd3b5ff4862553e318e3c4b9c
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2016-05-13 21:22:46 -0500

    dynamic marker for sellers

--- seller/views.py
+++ seller/views.py
@@ -2,7 +2,7 @@ from urllib2 import URLError
 
 from django.contrib.gis import geos
 from django.contrib.gis import measure
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.template import RequestContext
 
 from geopy.geocoders import GoogleV3
@@ -29,25 +29,28 @@ def get_client_ip(request):
 def get_client_location_with_ip(ip):
     url = 'http://ip-api.com/json/' + ip
     out = requests.get(url=url).json()
-    # return '%f %f' % (out['lon'], out['lat'])
     return out['lat'], out['lon']
 
 def home(request):
-    form = AddressForm()
-    sellers = []
-
-    ip = get_client_ip(request)
-    ip = '142.33.135.231'
-    # location = get_client_location_with_ip(ip)
-    lat, lon = get_client_location_with_ip(ip)
-
     if request.POST:
         form = AddressForm(request.POST)
         if form.is_valid():
-            location = form.cleaned_data['address']
-            sellers = get_sellers(location)
+            location2 = form.cleaned_data['address']
+            sellers = get_sellers(location2)
+            lnglat = location2.split(' ')
+            location1 = '%s,%s' % (lnglat[1], lnglat[0])
+    else:
+        form = AddressForm()
+        sellers = []
+        ip = get_client_ip(request)
+        ip = '142.33.135.231'
+        lat, lon = get_client_location_with_ip(ip)
+        location1 = '%f, %f' % (lat, lon)
+        location2 = '%f %f' % (lon, lat)    # geopy location
 
-    return render_to_response(
-        'home.html',
-        {'form': form, 'sellers': sellers, 'location1': '%f, %f' % (lat, lon), 'location2': '%f %f' % (lon, lat)},
-        context_instance=RequestContext(request))
+    return render(request, 'home.html', {
+        'form': form, 
+        'sellers': sellers, 
+        'location1': location1, 
+        'location2': location2
+    })
--- solo/settings.py
+++ solo/settings.py
@@ -29,6 +29,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
+
+MEDIA_URL = STATIC_URL + "media/"
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, *MEDIA_URL.strip("/").split("/"))
 
 # Application definition
 
@@ -67,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.static',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
@@ -124,4 +130,3 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
