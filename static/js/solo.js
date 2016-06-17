bf20c7c49da5a72000bfd06531644bbdf2576f58
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2016-06-17 15:47:47 -0500

    fix-10

--- static/js/solo.js
+++ static/js/solo.js
@@ -86,7 +86,31 @@ $(function(){
 	});
 	}
 
-	videojs('landing-video').ready(function() {
-	    this.play();
-	});
+	// videojs('landing-video').ready(function() {
+	// 	console.log('start');
+	//     this.play();
+	// });
+
+	videojs.autoSetup();
+
+    videojs('landing-video').ready(function(){
+      console.log(this.options()); //log all of the default videojs options
+      
+       // Store the video object
+      var myPlayer = this, id = myPlayer.id();
+      // Make up an aspect ratio
+      var aspectRatio = 264/640; 
+
+      function resizeVideoJS(){
+        var width = document.getElementById(id).parentElement.offsetWidth;
+        // var width = 1920;
+        myPlayer.width(width).height( width * aspectRatio );
+
+      }
+      
+      // Initialize resizeVideoJS()
+      resizeVideoJS();
+      // Then on resize call resizeVideoJS()
+      window.onresize = resizeVideoJS; 
+    });	
 });
